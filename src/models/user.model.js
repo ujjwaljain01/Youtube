192281a6ba54cc6926d7b1a82619cce8ad513ed3
Ujjwal jain <ujjwaljainmain@gmail.com>
2026-04-22 20:24:37 +0530

    Complete production grade project setup

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,4 @@
-import mongoose, { mongo, Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  secure: true,
+});
+
+console.log(cloudinary.config());
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    // Upload the image
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("file uploaded succesfully", response);
+    console.log(response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    console.error(error);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
